77a34859c59889a1157454bf0effd08757b67867
xionglongxiang <xionglongxiang@126.com>
2022-11-28 18:04:41 +0800

    change text

--- packages/core/lib/core.js
+++ packages/core/lib/core.js
@@ -1,7 +1,7 @@
-'use strict';
+'use strict'
 
-module.exports = core;
+module.exports = core
 
-function core() {
-    return "Hello from core";
+function core () {
+  return 'Hello from core !!!'
 }
